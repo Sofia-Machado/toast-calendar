a90cb71c44875ecf92b635c48b02b0016b29dc98
Sofia Machado <sofia.machado@riva-office.pt>
2023-04-04 15:12:50 +0100

    toast calendar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+import { useCallback, useRef } from 'react';
+import Calendar from '@toast-ui/react-calendar';
+import '@toast-ui/calendar/dist/toastui-calendar.min.css';
+import 'tui-date-picker/dist/tui-date-picker.css';
+import 'tui-time-picker/dist/tui-time-picker.css';
 import './App.css';
 
-function App() {
+export default function App() {
+
+  const calendar = useRef();
+  /* const calendar = new Calendar('#container', {
+    useFormPopup: true,
+    useDetailPopup: true,
+    defaultView: 'day',
+  }); */
+
+  const options = {
+    useFormPopup: true,
+    useDetailPopup: true,
+    view: 'day',
+    
+};
+  const calendars = [{ id: 'cal1', name: 'Personal' }];
+  const initialEvents = [
+    {
+      id: '1',
+      calendarId: 'cal1',
+      title: 'Lunch',
+      category: 'time',
+      start: '2023-04-10T12:00:00',
+      end: '2023-04-10T13:30:00',
+    },
+    {
+      id: '2',
+      calendarId: 'cal1',
+      title: 'Coffee Break',
+      category: 'time',
+      start: '2023-04-28T15:00:00',
+      end: '2023-04-28T15:30:00',
+    },
+  ];
+
+  function moveToNextOrPrevRange(offset) {
+    calendar.current.calendarInstance.move(offset)
+  }
+  
+  function onClickTodayBtn() {
+    calendar.today();
+  }
+
+  const onClickNav = (ev) => {
+    if ((ev.target).tagName === 'BUTTON') {
+      const button = ev.target;
+      const actionName = (button.getAttribute('data-action') ?? 'month').replace('move-', '');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>🍞📅 TOAST UI Calendar + React.js</h1>
+      <div>
+        <span>
+          <button
+            type="button"
+            className="btn btn-default btn-sm move-today"
+            data-action="move-today"
+            onClick={onClickTodayBtn}
+          >
+            Today
+          </button>
+          <button
+            type="button"
+            className="btn btn-default btn-sm move-day"
+            data-action="move-prev"
+            onClick={() => moveToNextOrPrevRange(-1)}
+          >
+            Prev
+          </button>
+          <button
+            type="button"
+            className="btn btn-default btn-sm move-day"
+            data-action="move-next"
+            onClick={() => moveToNextOrPrevRange(1)}
+          >
+            Next
+          </button>
+        </span>
+        <span className="render-range">selectedDateRangeText</span>
+      </div>
+
+      <Calendar
+        ref={calendar}
+        {...options}
+        height="900px"
+        calendars={calendars}
+        events={initialEvents}
+    />
+    </>
   );
 }
-
-export default App;
